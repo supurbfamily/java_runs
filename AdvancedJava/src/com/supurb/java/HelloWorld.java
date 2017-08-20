577163a27d39023bf68b6db992f856cc46a594f0
supurbfamily <supurbfamily@gmail.com>
2017-08-20 13:02:31 -0400

    this is my next attempt to commit and check for file on the web repository.

--- AdvancedJava/src/com/supurb/java/HelloWorld.java
+++ AdvancedJava/src/com/supurb/java/HelloWorld.java
@@ -1,10 +1,17 @@
 package com.supurb.java;
 
+/**
+ * This i a simple Hello world class to check-out Git
+ * @author Subhojit
+ *
+ */
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("hello world ... this is my first file commit to git");
+		
 	}
 
 }
