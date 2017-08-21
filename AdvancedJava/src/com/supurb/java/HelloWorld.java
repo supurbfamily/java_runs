840316e997dfeed84dea1e00169f29609844245a
Subhojit <Subhojit@supurbsandbox>
2017-08-21 13:31:28 -0400

    Adding class InitBlocks and updated the Hello world Class to call it.

--- AdvancedJava/src/com/supurb/java/HelloWorld.java
+++ AdvancedJava/src/com/supurb/java/HelloWorld.java
@@ -1,6 +1,5 @@
 package com.supurb.java;
 
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -14,14 +13,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world ... this is my first file commit to git");
-		Logger myLogger =  Logger.getLogger("Subhojit's Logger");
-		myLogger.log(Level.INFO, "THis is my first calss with a logger in Git branch");
-		System.out.println("Wow! it worked.");
-		System.out.println("This should not be in the main, but in the branch");
-		System.out.println("One more time ... fixed config");
-		/* Lets see where it goes */
-		System.out.println("This goes to master");
-		/* this is the real test now that I have setup the local bracnhes appropriately and mapeed them to git */
+		InitBlocks myInitBlocks = new InitBlocks();
+		System.out.println(myInitBlocks.toString());
 	}
 
 }
---
+++ AdvancedJava/src/com/supurb/java/InitBlocks.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.supurb.java;
+
+/**
+ * @author Subhojit
+ *
+ */
+public class InitBlocks {
+	// this is an init block - 1
+	{
+		System.out.println("Printing from Init block - 1");
+	}
+	
+	public InitBlocks()
+	{
+		System.out.println("Printing from comstructor");
+	}
+	
+	// this is an init block - 2
+	{
+		System.out.println("Printing from Init block - 2");
+	}
+		
+}
